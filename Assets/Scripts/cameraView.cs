ba5ea6d2f5e2fed4176aca699c773de8b9ddd4af
onewkub <37097081+onewkub@users.noreply.github.com>
2019-07-10 00:37:27 +0700

    Initial commit

---
+++ Assets/Scripts/cameraView.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraView : MonoBehaviour
+{
+    private float xAxixClamp;
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        xAxixClamp = 0f;
+    }
+    [Range(10f, 100f)]
+    public float mouseSensetive = 40f;
+    public GameObject mainCam;
+    public GameObject PlayerBody;
+
+    private void Update()
+    {
+        CameraRotation();
+    }
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensetive;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensetive;
+        xAxixClamp += mouseY;
+        if(xAxixClamp > 90.0f)
+        {
+            xAxixClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if(xAxixClamp < -90.0f)
+        {
+            xAxixClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+
+        }
+        mainCam.transform.Rotate(Vector3.left * mouseY);
+        PlayerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+    private void ClampXAxisRotationToValue(float value)
+    {
+        Vector3 eulerRotation = mainCam.transform.eulerAngles;
+        eulerRotation.x = value;
+        mainCam.transform.eulerAngles = eulerRotation;
+    }
+}
