4f4c614f13e9a6bffc406d691ce4802b2351c75a
beam41 <beam_baimon@hotmail.com>
2019-07-13 23:44:59 +0700

    Add controller test

---
+++ Assets/ControllerTester.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerTester : MonoBehaviour
+{
+    public Text title;
+    public Text Axis0;
+    public Text Axis1;
+    public Text Axis2;
+    public Text Axis3;
+
+    private void Start()
+    {
+        string[] controller = Input.GetJoystickNames();
+        title.text = $"Axis text :3 ({controller[0]})";
+    }
+
+    void Update()
+    {
+        Axis0.text = $"Horizontal = {Input.GetAxis("Horizontal")}";
+        Axis1.text = $"Horizontal2 = {Input.GetAxis("Horizontal2")}";
+
+        Axis2.text = $"Vertical = {Input.GetAxis("Vertical")}";
+        Axis3.text = $"Vertical2 = {Input.GetAxis("Vertical2")}";
+    }
+}
