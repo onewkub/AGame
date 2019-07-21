4bbf4beb73684d70f162fba6f54ab91af0fc2e58
onewkub <37097081+onewkub@users.noreply.github.com>
2019-07-22 01:14:26 +0700

    add wheel rotation

--- Assets/Scripts/WheelSimpleMove.cs
+++ Assets/Scripts/WheelSimpleMove.cs
@@ -20,38 +20,63 @@ public class WheelSimpleMove : MonoBehaviour
     public void GetInput()
     {
         
-        if (Input.GetAxis(VerticalLeft) !=0 || Input.GetAxis(VerticalRight) !=0 )
+        if (Input.GetAxis(VerticalLeft) !=0f || Input.GetAxis(VerticalRight) !=0f )
         {
-            if (sinceLastMove <= 0)
+            if (sinceLastMove <= 0f)
             {
                 m_leftVertical = Input.GetAxis(VerticalLeft);
                 m_rightVertical = Input.GetAxis(VerticalRight);
-                Debug.Log($"{m_leftVertical} {m_rightVertical}");
+                //Debug.Log($"{m_leftVertical} {m_rightVertical}");
                 isBraking = false;
                 sinceLastMove = moveCooldown;
             }
             else if (sinceLastMove < moveCooldown - movingTime)
             {
-                isBraking = true;
-                m_leftVertical = 0;
-                m_rightVertical = 0;
+                //isBraking = true;
+                m_leftVertical = 0f;
+                m_rightVertical = 0f;
             }
         }
         #if UNITY_EDITOR
         //for debug (this move both wheel at once)
-        else if (Input.GetKey(KeyCode.Keypad8)){
-            m_leftVertical = 2;
-            m_rightVertical = 2;
-            isBraking = false;
-            sinceLastMove = moveCooldown;
+        else if (Input.GetAxis("RightTrigger") != 0f){
+            if (sinceLastMove <= 0f) {
+                m_leftVertical = Input.GetAxis("RightTrigger");
+                m_rightVertical = Input.GetAxis("RightTrigger");
+                isBraking = false;
+                sinceLastMove = moveCooldown;
+            }
+            else if(sinceLastMove < moveCooldown - movingTime)
+            {
+                //isBraking = true;
+                m_leftVertical = 0f;
+                m_rightVertical = 0f;
+            }
         }
         #endif
+        else if(Input.GetAxis("LeftTrigger") != 0f)
+        {
+            if (sinceLastMove <= 0f)
+            {
+                m_leftVertical = -Input.GetAxis("LeftTrigger");
+                m_rightVertical = -Input.GetAxis("LeftTrigger");
+                isBraking = false;
+                sinceLastMove = moveCooldown;
+            }
+            else if(sinceLastMove < moveCooldown - movingTime)
+            {
+                //isBraking = true;
+                m_leftVertical = 0f;
+                m_rightVertical = 0f;
+            }
+        }
+
         else
         {
-            sinceLastMove = 0;
+            sinceLastMove = 0f;
             isBraking = true;
-            m_leftVertical = 0;
-            m_rightVertical = 0;
+            m_leftVertical = 0f;
+            m_rightVertical = 0f;
         }
         sinceLastMove -= Time.deltaTime;
 
@@ -80,15 +105,35 @@ public class WheelSimpleMove : MonoBehaviour
 
     }
 
+    private void WheelsPoseUpdate()
+    {
+        WheelPoseUpdate(leftWheel, leftWheelT);
+        WheelPoseUpdate(rightWheel, rightWheelT);
+
+    }
+
+    private void WheelPoseUpdate(WheelCollider _collider, Transform _transform)
+    {
+        Vector3 _pos = _transform.position;
+        Quaternion _quat = _transform.rotation;
+        _collider.GetWorldPose(out _pos, out _quat);
+
+        _transform.position = _pos;
+        _transform.rotation = _quat;
+    }
     private void Update()
     {
         GetInput();
+
     }
 
     private void FixedUpdate()
     {
         WheelsMove();
-        BrakeWheel(); 
+        WheelsPoseUpdate();
+        BrakeWheel();
+        Debug.Log(sinceLastMove +"<"+ moveCooldown +"-"+ movingTime);
+        Debug.Log("DeltaTime: " + Time.deltaTime);
 
     }
 }
