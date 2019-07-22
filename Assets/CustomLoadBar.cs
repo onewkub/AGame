81ea61b5f3535cac14cf142bd20bd57b39dbefeb
beam41 <beam_baimon@hotmail.com>
2019-07-22 23:52:12 +0700

    New custom load bar because I hate builtin slider

---
+++ Assets/CustomLoadBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomLoadBar : MonoBehaviour
+{
+    public float LoadPercentage
+    {
+        set
+        {
+            delta = Mathf.Clamp01(value);
+        }
+    }
+    private float delta;
+
+    public RectTransform progressBar;
+    private float barWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barWidth = GetComponent<RectTransform>().rect.width;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        progressBar.sizeDelta = new Vector2((delta - 1) * barWidth, 0f);
+    }
+}
