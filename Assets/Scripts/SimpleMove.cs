eef058c6ed46a8e6945df6339a9bcb0476a86b25
onewkub <37097081+onewkub@users.noreply.github.com>
2019-08-01 00:23:42 +0700

    Add Testor and Clean someEdge

---
+++ Assets/Scripts/SimpleMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMove : MonoBehaviour
+{
+    public float walkSpeed;
+    CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        playerMovement();
+    }
+
+    private void playerMovement()
+    {
+        float HorizInput = Input.GetAxis("Horizontal");
+        float VertInput = Input.GetAxis("Vertical");
+        bool isWalking = !Mathf.Approximately(HorizInput, 0f) || !Mathf.Approximately(VertInput, 0f);
+        Vector3 moveForward = transform.forward * VertInput;
+        Vector3 moveSide = transform.right * HorizInput;
+
+        Debug.Log(HorizInput + " " + VertInput);
+
+
+        if (isWalking)
+        {
+                characterController.SimpleMove((moveForward + moveSide) * walkSpeed);
+        }
+    }
+}
