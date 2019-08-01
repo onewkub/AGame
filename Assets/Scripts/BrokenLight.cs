04afcc880e86a8819ca97a42a07bc46394d50b31
onewkub <37097081+onewkub@users.noreply.github.com>
2019-08-01 18:55:46 +0700

    Light Script

---
+++ Assets/Scripts/BrokenLight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrokenLight : MonoBehaviour
+{
+    private float delay;
+    private bool lightStatus = true;
+    public Light m_light;
+    private void Start()
+    {
+        m_light = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        float timeRand = Random.Range(0f, 1f);
+        
+    }
+}
