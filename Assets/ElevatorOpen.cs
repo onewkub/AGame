4041580878ff06d243ffbf338406f1b1c14989f5
onewkub <37097081+onewkub@users.noreply.github.com>
2019-08-03 01:09:52 +0700

    Add FlashLightASset and Make ElevatorOpenScript

---
+++ Assets/ElevatorOpen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorOpen : MonoBehaviour
+{
+    public GameObject ElDoor;
+    private Animator ElAnimator;
+    private void Awake()
+    {
+        ElAnimator = ElDoor.GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.transform.name == "Testor")
+        {
+            ElAnimator.SetBool("Open", true);
+            Debug.Log("Im in");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.transform.name == "Testor")
+        {
+            ElAnimator.SetBool("Open", false);
+            Debug.Log("im out");
+        }
+    }
+}
