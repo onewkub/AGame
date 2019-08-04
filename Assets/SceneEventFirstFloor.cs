dc00e71bca9d620895e5a16fd266df022511bfa5
onewkub <37097081+onewkub@users.noreply.github.com>
2019-08-04 20:56:47 +0700

    Try to move WHeelChair

---
+++ Assets/SceneEventFirstFloor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneEventFirstFloor : MonoBehaviour
+{
+    public static SceneEventFirstFloor Instance { get; set; }
+    private bool PassTutorial;
+    public GameObject OpenTrigger;
+    public GameObject CloseTrigger;
+    public GameObject InsideTrigger;
+
+    private void Start()
+    {
+        PassTutorial = false;
+        OpenTrigger.SetActive(false);
+        CloseTrigger.SetActive(false);
+    }
+
+
+}
