b6fb4a2b15c166079370a8ee2dff8fc84adfb729
onewkub <37097081+onewkub@users.noreply.github.com>
2019-08-13 22:39:21 +0700

    Add running Sound Second FLoor

--- Assets/Scripts/NurseMadnessSecondFloor.cs
+++ Assets/Scripts/NurseMadnessSecondFloor.cs
@@ -6,20 +6,23 @@ public class NurseMadnessSecondFloor : MonoBehaviour
     public NavMeshAgent agent;
     public bool running;
     public bool walking;
-
+    public AudioSource RunningSound;
     private void Update()
     {
         walking = isMoving();
 
         if (running && isMoving())
         {
-            agent.speed = 5f;
+            agent.speed = 8f;
             NurseMadnessController.Instance.Running();
+            if (!RunningSound.isPlaying)
+                RunningSound.Play();
         }
         else
         {
             agent.speed = 3.5f;
             NurseMadnessController.Instance.StopRunning();
+            RunningSound.Stop();
         }
 
         if (walking)
