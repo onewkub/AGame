1b45f9a0c924a4349828cbb40434a5f6c3206206
beam41 <beam_baimon@hotmail.com>
2019-08-13 20:42:23 +0700

    Remove unnecessary

--- Assets/Scripts/LoadNextScenF2.cs
+++ Assets/Scripts/LoadNextScenF2.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LoadNextScenF2 : MonoBehaviour
 {
-    private void OnLevelWasLoaded(int level)
+    private void Start()
     {
         SceneLoadManager.Instance.ElevatorLoadAsync("ElevatorScene");
 
