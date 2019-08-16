871f6a2131b6c919a2e3c547add06e2d32be055e
worawit <mon2350@hotmail.com>
2019-08-16 17:59:39 +0700

    Add some Script

---
+++ Assets/EZPath.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EZPath : MonoBehaviour
+{
+	public GameObject[] AllDest;
+
+	public Vector3 CurrentPos;
+
+	private void isArriveDest()
+	{
+
+	}
+}
